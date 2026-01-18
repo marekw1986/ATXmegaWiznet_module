12418c985e513dcdb89df582bc1c636ac6d578f5
Marek Wiecek <marekw1986@gmail.com>
2026-01-18 15:05:07 +0100

    str to ip

--- code/wolfmqtt/mqttnet.c
+++ code/wolfmqtt/mqttnet.c
@@ -11,6 +11,7 @@ static int NetConnect(void *context, const char* host, word16 port, int timeout_
 static int NetRead(void *context, byte* buf, int buf_len, int timeout_ms);
 static int NetWrite(void *context, const byte* buf, int buf_len, int timeout_ms);
 static int NetDisconnect(void *context);
+static uint8_t ip_str_to_bytes(const char *str, uint8_t* ip[4]);
 
 int MqttNetInit(MqttNet* net) {
     if (net) {
@@ -25,6 +26,10 @@ int MqttNetInit(MqttNet* net) {
 
 static int NetConnect(void *context, const char* host, word16 port, int timeout_ms) {
     uint8_t ip[4] = {192, 168, 1, 61};
+    
+    if (!ip_str_to_bytes(host, &ip)) {
+        return MQTT_CODE_ERROR_NETWORK;
+    }
     
     if (socket(MQTT_SOCKET, Sn_MR_TCP, 1883, 0) != MQTT_SOCKET) {
         return MQTT_CODE_ERROR_NETWORK;
@@ -71,4 +76,38 @@ static int NetDisconnect(void *context) {
     disconnect(MQTT_SOCKET);
     close(MQTT_SOCKET);
     return MQTT_CODE_SUCCESS;
+}
+
+#include <stdint.h>
+#include <stdbool.h>
+
+static uint8_t ip_str_to_bytes(const char *str, uint8_t* ip[4])
+{
+    uint8_t i = 0;
+    uint16_t val = 0;
+
+    while (*str) {
+        if (*str >= '0' && *str <= '9') {
+            val = val * 10 + (*str - '0');
+            if (val > 255) {
+                return 0;   // overflow
+            }
+        } else if (*str == '.') {
+            if (i >= 3) {
+                return 0;   // too many dots
+            }
+            ip[i++] = (uint8_t)val;
+            val = 0;
+        } else {
+            return 0;       // invalid character
+        }
+        str++;
+    }
+
+    if (i != 3) {
+        return false;           // not enough octets
+    }
+
+    ip[i] = (uint8_t)val;
+    return 1;
 }
